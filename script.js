1f7c09cc0e3fbd369584914d799fbbf510c791aa
MadMaxCUX <70437367+MadMaxCUX@users.noreply.github.com>
2024-06-10 17:41:28 +0200

    fixxingggg

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-window.addEventListener("load", () => {
+document.addEventListener("DOMContentLoaded", (event) => {
     const videoElement = document.getElementById("background-video");
-    if (videoElement) {
-      videoElement.muted = false; // Ensure the video is unmuted
+    const playButton = document.getElementById("play-button");
+  
+    function playVideoWithSound() {
+      videoElement.muted = false; // Unmute the video
       videoElement.play()
         .then(() => {
           console.log("Video is playing with sound");
+          playButton.style.display = 'none'; // Hide play button
         })
         .catch((error) => {
           console.error("Error attempting to play video with sound:", error);
+        });
+    }
+  
+    if (videoElement) {
+      videoElement.muted = true; // Start with the video muted
+      videoElement.play()
+        .then(() => {
+          console.log("Video is playing muted");
+          playButton.style.display = 'block'; // Show play button
+          playButton.addEventListener("click", playVideoWithSound);
+        })
+        .catch((error) => {
+          console.error("Error attempting to play video:", error);
         });
     } else {
       console.error("Video element not found");
