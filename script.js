57a37a62d79ac39a08eba422f3678ffd59284b29
MadMaxCUX <70437367+MadMaxCUX@users.noreply.github.com>
2024-06-10 17:31:09 +0200

    sound fix (again)

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     const videoElement = document.getElementById("background-video");
     if (videoElement) {
-      videoElement.muted = false; // Start with the video muted
-      videoElement
-        .play()
+      videoElement.muted = false; // Ensure the video is unmuted
+      videoElement.play()
         .then(() => {
-          console.log("Video is playing");
+          console.log("Video is playing with sound");
         })
         .catch((error) => {
-          console.error("Error attempting to play video:", error);
+          console.error("Error attempting to play video with sound:", error);
         });
     } else {
       console.error("Video element not found");
